36c90cb92dc1e435204b5e50a467721cf1b87eaf
THEUN.EN <THEUN.EN@PNC.LAN>
2024-04-26 13:24:56 +0700

    #2 merge file from main

--- src/Passenger.ts
+++ src/Passenger.ts
@@ -1,3 +1,40 @@
-export class Passenger {
+import { Meal } from "./Meal";
+import { Seat } from "./Seat";
 
+export class Passenger {
+    private mealReferences: Meal[];
+    private seatNames: Seat[];
+
+    constructor(
+        private name: string,
+        private frequentFlyerNumber: string,
+        private passportNumber: string
+    ) {
+        this.seatNames = [];
+        this.mealReferences = [];
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getSeatNames(): Seat[] {
+        return this.seatNames;
+    }
+
+    addSeatName(seatName: Seat): void {
+        this.seatNames.push(seatName);
+    }
+
+    addMealReference(mealReference: Meal): void {
+        this.mealReferences.push(mealReference);
+    }
+
+    getFrequentFlyerNumber(): string {
+        return this.frequentFlyerNumber;
+    }
+
+    getPassportNumber(): string {
+        return this.passportNumber;
+    }
 }
---
+++ src/Ticket.ts
@@ -0,0 +1,7 @@
+export class Ticket {
+    
+    private bookingTrips: Book
+    constructor(private id: string) {
+        this.id = id;
+    }
+}
