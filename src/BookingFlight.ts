80dbfe2ec08c029d43b82db4e4fac00ebbc2b90d
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-26 19:54:21 +0700

    push code BoookingFlight #3

--- src/BookingFlight.ts
+++ src/BookingFlight.ts
@@ -1,10 +1,16 @@
+import { Airport } from "./Airport";
+import { Baggage } from "./Baggage";
+import { Meal } from "./Meal";
+import { Passenger } from "./Passenger";
+import { Seat } from "./Seat";
+
 class BookingFlight {
     private departureAirport: Airport;
     private arrivalAirport: Airport;
-    passengers: Passenger;
-    baggage: Baggage[] = [];
-    meals: Meal[] = [];
-    seats: Seat[] = [];
+    private passengers: Passenger[] = [];
+    private baggage: Baggage[] = [];
+    private meals: Meal[] = [];
+    private seats: Seat[] = [];
     constructor(
         private flightNumber: number,
         private departureTime: Date,
@@ -14,15 +20,48 @@ class BookingFlight {
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
     }
-}
-
-getFlightNumber(): number { 
-}
-
-getDepartureCity(): string {
-
-}
-
-arrivalAirport(): string {
     
+    getFlightNumber(): number {
+        return this.flightNumber;
+    }
+
+    getDepartureTime(): Date {
+        return this.departureTime;
+    }
+
+    setDepartureTime(departureTime: Date): void {
+        this.departureTime = departureTime;
+    }
+
+    getArrivalTime(): Date {
+        return this.arrivalTime;
+    }
+
+    setArrivalTime(arrivalTime: Date): void {
+        this.arrivalTime = arrivalTime;
+    }
+
+    getDepartureAirport(): Airport {
+        return this.departureAirport;
+    }
+
+    setDepartureAirport(departureAirport: Airport): void {
+        this.departureAirport = departureAirport;
+    }
+
+    getArrivalAirport(): Airport {
+        return this.arrivalAirport;
+    }
+
+    setArrivalAirport(arrivalAirport: Airport): void {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    addPassenger(passenger: Passenger): void {
+        this.passengers.push(passenger);
+    }
+
+    removePassenger(passenger: Passenger): void {
+        this.passengers = this.passengers.filter(p => p !== passenger);
+    }
 }
