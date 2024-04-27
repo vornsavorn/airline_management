6b909a51cca8cbc7b99450cc1be6490ca5ebc85a
LAYHOR.MET <LAYHOR.MET@PNC.LAN>
2024-04-27 10:40:42 +0700

    push code to rpot #7

---
+++ src/Route.ts
@@ -0,0 +1,31 @@
+import { Airport } from "./Airport";
+
+export class Route {
+    private origin : Airport;
+    private destination : Airport;
+    constructor(
+        private departureTime: Date,
+        private arrivalTime: Date,
+        private duration: string
+    ){
+       this.departureTime = departureTime;
+       this.arrivalTime = arrivalTime;
+       this.duration = duration;
+    }
+    public getOrigin() : Airport {
+        return this.origin;
+    }
+    public getDestination() : Airport {
+        return this.destination;
+    }
+    public getDepartureTime() : Date {
+        return this.departureTime;
+    }
+    public getArrivalTime() : Date {
+        return this.arrivalTime;
+    }
+    public getDuration() : string {
+        return this.duration;
+    }
+
+}
