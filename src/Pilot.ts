4f66ec8be7387f31b63c910089fc45df074be710
thary.oeun <thary.oeun@student.passerellernumeriques.org>
2024-04-28 12:36:04 +0700

    #8 return ticket

--- src/Pilot.ts
+++ src/Pilot.ts
@@ -1,13 +1,13 @@
 import { Flight } from "./Flight";
 
-class AirlinePilot {
-    constructor(
-        private id: number,
-        private name: string,
-        private flights: Flight[]
-    ){
+export class Pilot {
+    constructor(private id: string, private name: string, private role: string, private age: number, private salary: number, private flights: Flight[]){
         this.id = id;
         this.name = name;
-        this.flights = flights;
+        this.role = role;
+        this.age = age;
+        this.salary = salary;
+        this.flights = [];
     }
+   
 }
