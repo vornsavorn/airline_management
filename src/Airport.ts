6b19c7423f94c0335dbf3c58e60bdae7c7d4a3b9
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-28 15:13:13 +0700

    push code Airport #9

--- src/Airport.ts
+++ src/Airport.ts
@@ -1,3 +1,9 @@
 export class Airport {
-    
+    constructor (
+        private code: string,
+        private name: string 
+    ) {
+        this.code = code;
+        this.name = name;
+    }  
 }
