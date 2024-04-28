caffff393caacca2472ed4d4b08e49079bc71611
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-28 11:17:08 +0700

    pull code

--- src/Flight.ts
+++ src/Flight.ts
@@ -1,8 +1,11 @@
+import { AirlinePilot } from "./Pilot";
 import { Route } from "./Route";
+import { Ticket } from "./Ticket";
 
 export class Flight {
     private routes: Route[] = [];
-    private polit: 
+    private polit: AirlinePilot;
+    private tickets: Ticket[] = [];
     constructor (
         private flightId: string,
         private flightName: string,
@@ -16,4 +19,52 @@ export class Flight {
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
     }
+
+    getFlightName(): string {
+        return this.flightName;
+    }
+
+    getDate(): Date {
+        return this.departureTime;
+    }
+
+    getFlightId(): string {
+        return this.flightId;
+    }
+
+    addRoute(route: Route) {
+        this.routes.push(route);
+    }
+
+    getRoutes(): Route[] {
+        return this.routes;
+    }
+
+    getPilot(): AirlinePilot {
+        return this.polit;
+    }
+
+    setPilot(pilot: AirlinePilot) {
+        this.polit = pilot;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+    getDepartureTime(): Date {
+        return this.departureTime;
+    }
+    getArrivalTime(): Date {
+        return this.arrivalTime;
+    }
+    getDuration(): number {
+        return this.arrivalTime.getTime() - this.departureTime.getTime();
+    }
+    addTicket(ticket: Ticket) {
+        this.tickets.push(ticket);
+    }
+    getTickets(): Ticket[] {
+        return this.tickets;
+    }
+    
 }
