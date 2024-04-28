af26701dca466677e05b1f84b6dbff2045cab412
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-28 15:30:10 +0700

    pull code

--- src/Airport.ts
+++ src/Airport.ts
@@ -1,4 +1,9 @@
+import { CrewMember } from "./CrewMember";
+import { Employee } from "./Employee";
+
 export class Airport {
+    private employees: Employee[] = [];
+    private crewMember: CrewMember[] = []; 
     constructor (
         private code: string,
         private name: string 
@@ -6,4 +11,34 @@ export class Airport {
         this.code = code;
         this.name = name;
     }  
+
+    getCode(): string {
+        return this.code;
+    }
+    getName(): string {
+        return this.name;
+    }
+    addEmployee(employee: Employee): void {
+        this.employees.push(employee);
+    }
+    addCrewMember(crewMember: CrewMember): void {
+        this.crewMember.push(crewMember);
+    }
+    getEmployees(): Employee[] {
+        return this.employees;
+    }
+    getCrewMember(): CrewMember[] {
+        return this.crewMember;
+    }
+    getNumberOfEmployees(): number {
+        return this.employees.length;
+    }
+    getNumberOfCrewMembers(): number {
+        return this.crewMember.length;
+    }
+    getNumberOfEmployeesAndCrewMembers(): number {
+        return this.employees.length + this.crewMember.length;
+    }
+   
+
 }
